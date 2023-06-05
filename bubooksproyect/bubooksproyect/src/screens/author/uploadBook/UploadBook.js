82430255817e09a4b1a21d66472e0f6c22326f55
Cristina Moreno Bermejo <93800790+Crismober@users.noreply.github.com>
2023-06-05 20:08:02 +0200

    Update UploadBook.js

--- bubooksproyect/bubooksproyect/src/screens/author/uploadBook/UploadBook.js
+++ bubooksproyect/bubooksproyect/src/screens/author/uploadBook/UploadBook.js
@@ -1,10 +1,9 @@
-import React, {useState} from 'react';
 import axios from 'axios';
 import {useNavigate} from "react-router-dom";
 import './UploadBook.css';
 import HeaderAuthor from "../../../components/header/HeaderAuthor";
 import url from '../../../environment'
-
+import {useState} from "react";
 const CreateBookForm = () => {
     const navigate = useNavigate();
     const [title, setTitle] = useState('');
@@ -22,6 +21,7 @@ const CreateBookForm = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+
 
         const bookData = {
             title,
@@ -56,7 +56,7 @@ const CreateBookForm = () => {
 
     return (
         <div className="uploadBookPage">
-            <HeaderAuthor /> {/* Render the author header */}
+            <HeaderAuthor/>
             <div className="uploadBookPageMain">
                 <h1>Edit your book</h1>
                 <form className="uploadBookForm" onSubmit={handleSubmit}>
@@ -76,7 +76,18 @@ const CreateBookForm = () => {
                         onChange={(e) => setLanguage(e.target.value)}
                         required
                     >
-                        {/* Language options */}
+                        <option value="">Select Language</option>
+                        <option value="ES">Spanish</option>
+                        <option value="EN">English</option>
+                        <option value="FR">French</option>
+                        <option value="DE">German</option>
+                        <option value="JA">Japanese</option>
+                        <option value="KO">Korean</option>
+                        <option value="RU">Russian</option>
+                        <option value="PT">Portuguese</option>
+                        <option value="ZH">Chinese</option>
+                        <option value="IT">Italian</option>
+                        <option value="HI">Hindi</option>
                     </select>
 
                     <label htmlFor="synopsis">Synopsis:</label>
@@ -86,6 +97,7 @@ const CreateBookForm = () => {
                         onChange={(e) => setSynopsis(e.target.value)}
                         required
                     ></textarea>
+
 
                     <label htmlFor="series">Series:</label>
                     <input
@@ -112,7 +124,13 @@ const CreateBookForm = () => {
                         onChange={(e) => setTargetAudience(e.target.value)}
                         required
                     >
-                        {/* Target audience options */}
+                        <option value="">Select Target Audience</option>
+                        <option value="0-5">Baby</option>
+                        <option value="5-10">Kid</option>
+                        <option value="10-15">Early Adolescence</option>
+                        <option value="15-20">Teenagers</option>
+                        <option value="20-25">Young Adult</option>
+                        <option value="25+">Adult</option>
                     </select>
 
                     <label htmlFor="matureContent">Mature Content:</label>
@@ -122,7 +140,8 @@ const CreateBookForm = () => {
                         onChange={(e) => setMatureContent(e.target.value === 'Y')}
                         required
                     >
-                        {/* Mature content options */}
+                        <option value="Y">Yes</option>
+                        <option value="N">No</option>
                     </select>
 
                     <label htmlFor="price">Price:</label>
@@ -142,11 +161,11 @@ const CreateBookForm = () => {
                         onChange={(e) => setBookCover(e.target.value)}
                         required
                     />
-
                     <button type="submit">Create Book</button>
                 </form>
             </div>
         </div>
+
     );
 };
 
