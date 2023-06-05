9000599bba85f5b5e05068dcdb2108d82bdecdd1
Crismober <cristinamorenobermejo1997@gmail.com>
2023-06-05 17:22:57 +0200

    user book page screen created

--- bubooksproyect/bubooksproyect/src/App.js
+++ bubooksproyect/bubooksproyect/src/App.js
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import NotFound from './screens/NotFound';
 import Main from "./screens/main/Main";
 import AuthorUserProfile from "./screens/user/authorProfile/AuthorProfile";
+import BookPage from "./screens/user/bookPage/BookPage";
 
 function App() {
     function App() {
@@ -10,6 +11,7 @@ function App() {
                 <Route path='*' element={<NotFound/>}/>
                 <Route path='/' element={<Main/>}/>
                 <Route path="/author/:alias" element={<AuthorUserProfile/>}/>
+                <Route path='bookPage/:id' element={<BookPage/>}/>
             </Routes>
         );
     }
